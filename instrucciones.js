69e43bd62506f6a07248c7147658f3aac0e8bf09
Jhon Dragon Ball <na_plascencia_erick_2023@cetis3.edu.mx>
2026-04-29 17:52:55 +0200

    Update instrucciones.js

--- instrucciones.js
+++ instrucciones.js
@@ -1,47 +1,54 @@
 //declaramos las variables
 
-let tesoro Math.floor(Math.random()*9)
-let intentos 3;
+let tesoro = Math.floor(Math.random() * 9);
+let intentos = 3;
 
-//se crea la función para Identificar una celda
+//se crea la funcion para identificar una celda
 
-function verificar (posicion) {
-let celdas document.getElementsByClassName("cell");
+function verificar(posicion) {
+  let celdas = document.getElementsByClassName("cell");
 
- IF (intentos === 0) return;
+// Evita seguir juganfo si ya terminó
+if (intentos === 0) return;
 
+//Evita repetir clic en misma celda
+if (celdas[posicion].style.pointerEvents === "none") return;
 
-if (celdas [posicion].style.pointerEvents === "none") return;
+// se descuenta intento de 1 en 1, hasta llegar a 0
+intentos--;
 
-  intentos--;
+  
+// se prepara la condicion de comparar la posicion del mouse contra la de donde esta el tesoro
+if (posicion === tesoro) {
+  celdas[posicion].style.backgroundColor = "gold";
+  celdas[posicion].textContent = "💎";
+  document.getElementById("mensaje").textContent = "!Felicidades encontraste el tesoro! ";
+  desactivarTodo();
 
-If (posicion === tesoro) {
+}else {
+  celdas[posicion].style.backgroundColor = "gray";
+  celdas[posicion].textContent  ="❌";
+  celdas[posicion].style.pointerEvents = "none";
 
-celdas [posicion].style.backgroundColor = "gold";
-celdas [posicion] textContent ="💎";
-document.getElementById ("mensaje").textContent = "¡Felicidades encontraste el tesoro!";
-desactivarTodo();
-} else {
-celdas [posicion].style.backgroundColor "gray";
-  celdas [posicion].textContent" ="❌";
-coldas [posicion].style.pointerEvents = "none";
-
-if (intentos === 0){
-document.getElementById("mensaje").textContent ="💀 ¡uy! Lo sient0 perdiste";
-mostrarTesoro();
-desactivarTodo();
-} else {
-document.getElementById("mensaje").textContent=
-"Sigue buscando... Intentos restantes: "+ intentos;
+  if (intentos === 0) {
+    document.getElementById("mensaje").textContent = "💀 !Uy! Lo siento perdiste";
+    mostrarTesoro();
+    desactivarTodo();
+  } else {
+    document.getElementById("mensaje").textContent =
+      "Sigue buscando... Intentos restantes: " + intentos;
+  }
+ }
 }
-}
-}
-
+// se llama a la celda que contiene el tesoro y se muestra el elemento grafico
 function mostrarTesoro(){
-let celdas = document.getElementsByClassName("cell");
-celdas [tesoro].textContent ="💎";
-celdas [tesoro].style.backgroundColor = "gold";
+    let celdas = document.getElementsByClassName("cell");
+    celdas[tesoro].textContent = "💎";
+    celdas[tesoro].style.backgroundColor = "gold";
+}
+
+//se recarga la pagina para reiniciar el juego
 
 function reiniciarJuego(){
-  location.reload();
+   location.reload();
 }
